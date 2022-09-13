ceff538e68b65ea9786a3f723544dc62219ed05e
Omar Samy <omar.samy3333@gmail.com>
2022-09-13 16:52:41 +0200

    First commit

--- IFCDataExtraction/Program.cs
+++ IFCDataExtraction/Program.cs
@@ -23,57 +23,7 @@ namespace IFCDataExtraction
             using (var stepModel = IfcStore.Open($"{filePath}{fileName}.ifc"))
             {
                 //save as XML
-                //stepModel.SaveAs($"{filePath}{fileName}.ifcxml");
-
-                // var col = stepModel.Instances.OfType<IfcRoot>().Where(ins => ins.GlobalId == "0Loq$KqEX8kx0FfhUNoU4z").FirstOrDefault() as IfcColumnStandardCase;             
-                //var col_material = ((col.HasAssociations.FirstOrDefault() as IfcRelAssociatesMaterial).RelatingMaterial as IfcMaterialProfileSetUsage).ForProfileSet;
-                //var product_all = stepModel.Instances;
-                //var product = (stepModel.Instances.OfType<IfcColumn>().FirstOrDefault().Representation.Representations.FirstOrDefault().Items.FirstOrDefault() as IfcExtrudedAreaSolid).SweptArea as IfcRectangleProfileDef;
-                
-                var product = (stepModel.Instances.OfType<IfcColumnStandardCase>().LastOrDefault().Representation.Representations.FirstOrDefault().Items.FirstOrDefault() as IfcExtrudedAreaSolid).SweptArea as IfcIShapeProfileDef;
-                var FlangeThickness = product.FlangeThickness;
-                var WebThickness = product.WebThickness;
-                var X = product.Position.Location.X;
-                var y = product.Position.Location.Y;
-                
-                //var xDim = product.XDim;
-                //var yDim = product.YDim;
-
-                //Console.WriteLine($"XDim={xDim}");
-                //Console.WriteLine($"YDim={yDim}");
-
-                //var product = stepModel.Instances.OfType<IfcMaterial>().Where(mt => mt.Name == "4000Psi").FirstOrDefault().HasProperties.FirstOrDefault().Properties;
-
-
-                //open transaction for changes
-                // using (var txn = stepModel.BeginTransaction("Doors modification"))
-                // {
-
-                //(product.Where(pr => pr.Name == "MassDensity").FirstOrDefault() as IfcPropertySingleValue).NominalValue = new IfcMassDensityMeasure(50000);
-                //product.XDim = 600;
-                //product.YDim = 600;
-                //product.ProfileName = "600x600";
-                //commit changes
-                //txn.Commit();
-                //}
-
-                //stepModel.SaveAs($"{filePath}{fileName}material_moded.ifc");
-                //Console.WriteLine(product);
-
-                ////open XML file
-                //using (var xmlModel = IfcStore.Open("SampleHouse.ifcxml"))
-                //{
-                //    //just have a look that it contains the same number of entities and walls.
-                //    var stepCount = stepModel.Instances.Count;
-                //    var xmlCount = xmlModel.Instances.Count;
-
-                //    var stepWallsCount = stepModel.Instances.CountOf<IIfcWall>();
-                //    var xmlWallsCount = xmlModel.Instances.CountOf<IIfcWall>();
-
-                //    Console.WriteLine($"STEP21 file has {stepCount} entities. XML file has {xmlCount} entities.");
-                //    Console.WriteLine($"STEP21 file has {stepWallsCount} walls. XML file has {xmlWallsCount} walls.");
-
-                //}
+                stepModel.SaveAs($"{filePath}{fileName}.ifcxml");
             }
         }
     }
